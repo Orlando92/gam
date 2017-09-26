e9cdad210da44fad5349a032f9e7ad54a6e65a94
Orlando <marito_1110@hotmail.com>
2017-09-25 19:45:39 -0500

    M

--- js/controller/partial/inicioCtrl.js
+++ js/controller/partial/inicioCtrl.js
@@ -1,5 +1,30 @@
 app.controller('inicioCtrl', ['$scope', function($scope){
+
+	$(document).ready(function(){
+	    $('.customer-logos').slick({
+	        slidesToShow: 5	,
+	        slidesToScroll: 1,
+	        autoplay: true,
+	        autoplaySpeed: 1000,
+	        arrows: false,
+	        dots: false,
+	        pauseOnHover: true,
+	        responsive: [{
+	            breakpoint: 876,
+	            settings: {
+	                slidesToShow: 4
+	            }
+	        }, {
+	            breakpoint: 520,
+	            settings: {
+	                slidesToShow: 3
+	            }
+	        }]
+	    });
+	});
 	
 	$('.parallax-window').parallax({imageSrc: 'img/pexels-photo-355056.jpeg'});
+
+
 
 }])
